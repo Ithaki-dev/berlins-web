20181e4408a269118220ccb9473485c4d73f41ef
rquesada <rquesadaqq@outlook.com>
2026-03-04 22:37:24 -0600

    Integrate Festivals component and update background color to dark theme

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,19 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Menu from './components/Menu';
 import Experience from './components/Experience';
+import Festivals from './components/Festivals';
 import Gallery from './components/Gallery';
 import Reservations from './components/Reservations';
 import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="min-h-screen bg-black">
+    <div className="min-h-screen bg-dark-500">
       <Navbar />
       <Hero />
       <Menu />
       <Experience />
+      <Festivals />
       <Gallery />
       <Reservations />
       <Footer />
